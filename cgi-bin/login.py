ca522a646d5dbc65fa9e06b94c4c86e350722598
user <user@dne.dne>
2019-01-28 21:13:45 -0800

    + add cookies & extract common cgi into base class

--- cgi-bin/login.py
+++ cgi-bin/login.py
@@ -3,17 +3,16 @@
 import mysql.connector
 import cgi
 import cgitb
+from os import environ
 
 
-class Login(object):
-    q_is_registered = 'select visits from login where userid = "%s"'
-    q_valid_login = 'select visits from login where userid = "%s" and password = "%s"'
-    q_insert_user = 'insert into login (userid, password, visits) values ("%s", "%s", "1")'
-    q_update_user = 'update login set visits = visits + 1 where userid = "%s" and password = "%s"'
-
-    def __init__(self, pw):
+class BaseCGI(object):
+    def __init__(self, pw, title):
         cgitb.enable()
         self.connection = None
+        self.headers = ["Content-Type: text/html"]
+        self.body = ["<TITLE>%s</TITLE>" % title]
+
         try:
             # connect to database
             self.connection = mysql.connector.connect(user='root',
@@ -21,24 +20,45 @@ class Login(object):
                                                       database='cst363',
                                                       host='127.0.0.1')
             # do the work
-            self.run()
+            self._run()
         except mysql.connector.Error as error:
-            self.print_header()
-            self.print_message("Error -- %s" % error)
-            self.connection.close()
-            raise error
+            self.add_tag("Error -- %s" % error)
         finally:
             self.connection.close()
+            self.respond()
+
+    def _run(self):
+        raise NotImplementedError
+
+    def respond(self):
+        for header in self.headers:
+            print(header)
+        print()  # blank line required, end of headers
+        for tag in self.body:
+            print(tag)
+
+    def add_tag(self, message):
+        self.body.append("<p>%s</p><br/>" % message)
 
     @staticmethod
-    def print_header():
-        print("Content-Type: text/html")  # HTML is following
-        print()  # ------------------------ blank line required, end of headers
-        print("<TITLE>CST363 homework 1</TITLE>")
+    def get_cookies():
+        if 'HTTP_COOKIE' in environ:
+            return environ['HTTP_COOKIE'].split(';')
+        else:
+            return dict()
 
-    @staticmethod
-    def print_message(message):
-        print("<p>%s</p><br/>" % message)
+    def add_cookie_to_header(self, key, value):
+        self.headers.append("Set-Cookie: %s=%s" % (key, value))
+
+
+class Login(BaseCGI):
+    q_is_registered = 'select visits from login where userid = "%s"'
+    q_valid_login = 'select visits from login where userid = "%s" and password = "%s"'
+    q_insert_user = 'insert into login (userid, password, visits) values ("%s", "%s", "1")'
+    q_update_user = 'update login set visits = visits + 1 where userid = "%s" and password = "%s"'
+
+    def __init__(self, pw):
+        super().__init__(pw, "CST363 SERIOUS BUSINESS project 1")
 
     def query_get_first(self, query):
         c = self.connection.cursor()
@@ -50,13 +70,15 @@ class Login(object):
         c.execute(query)
         self.connection.commit()
 
-    def run(self):
-        self.print_header()
-
+    def _run(self):
         form = cgi.FieldStorage()
+
+        for cookie in self.get_cookies():
+            self.add_tag(str(cookie))
 
         # retrieve input values
         userid = form["userid"].value
+        self.add_cookie_to_header("user", userid)
         password = form["password"].value
 
         if 'register' in form:
@@ -64,7 +86,7 @@ class Login(object):
         elif 'login' in form:
             self.login_user(userid, password)
         else:
-            self.print_message("ERROR -- not register or login")
+            self.add_tag("ERROR -- not register or login")
 
     def register_user(self, userid, password):
         # does user already exist?
@@ -72,11 +94,12 @@ class Login(object):
 
         if user_visits is not None:
             # yes -> ask for different name
-            self.print_message("Sorry, the username: '%s' is already in use." % userid)
+            self.add_tag("Sorry, the username: '%s' is already in use." % userid)
         else:
             # no -> add new user, thank them
             self.query_committed(self.q_insert_user % (userid, password))
-            self.print_message("Thank you for registering, %s." % userid)
+            self.add_tag("Thank you for registering, %s." % userid)
+        # self.add_cookie_to_header(userid, password)
 
     def login_user(self, userid, password):
         # is username & password valid?
@@ -84,12 +107,12 @@ class Login(object):
 
         if user_visits is None:
             # no -> incorrect
-            self.print_message("Sorry, either the provided username or password was not correct")
+            self.add_tag("Sorry, either the provided username or password was not correct")
         else:
             # yes -> thank them, show visit count
             visits = user_visits[0] + 1
             self.query_committed(self.q_update_user % (userid, password))
-            self.print_message("Thank you for visiting again, %s. This is visit number %s." % (userid, str(visits)))
+            self.add_tag("Thank you for visiting again, %s. This is visit number %s." % (userid, str(visits)))
 
 
 Login('password')
